3e4fdd9e5ee20a975587a757088f76a2865c4c6e
Jessica-Thomas <jessica.thomas648@gmail.com>
2021-04-19 11:00:56 -0400

    added classes and more functions

--- snake.py
+++ snake.py
@@ -2,55 +2,71 @@ import pygame
 from pygame.locals import *
 
 
-# function to draw the block
-def draw_block():
-    surface.fill((97, 24, 44))
-    surface.blit(block, (block_x, block_y))
-    pygame.display.flip()
+class Snake:
+    def __init__(self, parent_screen):
+        self.parent_screen = parent_screen
+        self.block = pygame.image.load("media/block.jpg").convert()
+        self.x = 100
+        self.y = 100
+
+    def draw(self):
+        self.parent_screen.fill((97, 24, 44))
+        self.parent_screen.blit(self.block, (self.x, self.y))
+        pygame.display.flip()
+
+    def move_up(self):
+        self.y -= 10
+        self.draw()
+
+    def move_down(self):
+        self.y += 10
+        self.draw()
+
+    def move_left(self):
+        self.x -= 10
+        self.draw()
+
+    def move_right(self):
+        self.x += 10
+        self.draw()
+class Game:
+    def __init__(self):
+        pygame.init()
+        # creating the game window
+        self.surface = pygame.display.set_mode((500, 500))
+        # gives a background color using RGB
+        self.surface.fill((97, 24, 44))
+        # creating the snake object within the game class
+        self.snake = Snake(self.surface)
+        self.snake.draw()
+
+    def run(self):
+        running = True
+
+        # event loop integral to any game/program
+        while running:
+            for event in pygame.event.get():
+                if event.type == KEYDOWN:
+                    # breaks infinite while loop to close game
+                    if event.key == K_ESCAPE:
+                        running = False
+                    #keystroke actions          
+                    if event.key == K_UP:
+                        self.snake.move_up()
+
+                    if event.key == K_DOWN:
+                        self.snake.move_down()
+
+                    if event.key == K_LEFT:
+                        self.snake.move_left()
+
+                    if event.key == K_RIGHT:
+                        self.snake.move_right()
+                            
+                # breaks infinite while loop to close game
+                elif event.type == QUIT:
+                    running = False
 
 if __name__ == "__main__":
-    pygame.init()
-
-    # creating the game window
-    surface = pygame.display.set_mode((500, 500))
-    # gives a background color using RGB
-    surface.fill((97, 24, 44))
-
-    # creating border
-    block = pygame.image.load("media/block.jpg").convert()
-    block_x = 100
-    block_y = 100
-    surface.blit(block, (block_x, block_y))
-
-
-    # Updates the display
-    pygame.display.flip()
-
-
-    running = True
-
-    # event loop integral to any game/program
-    while running:
-        for event in pygame.event.get():
-            if event.type == KEYDOWN:
-                # breaks infinite while loop to close game
-                if event.key == K_ESCAPE:
-                    running = False
-                #keystroke actions          
-                if event.key == K_UP:
-                    block_y -= 10
-                    draw_block()
-                if event.key == K_DOWN:
-                    block_y += 10
-                    draw_block()
-                if event.key == K_LEFT:
-                    block_x -= 10
-                    draw_block()
-                if event.key == K_RIGHT:
-                    block_x += 10
-                    draw_block()
-
-                         
-            # breaks infinite while loop to close game
-            elif event.type == QUIT:
-                running = False
+    game = Game()
+    game.run()
